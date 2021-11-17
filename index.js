a9814678b6a0a04f1fc2def707f055b218299f86
alexible <aleksandrkr7k@gmail.com>
2021-11-16 17:34:43 -0800

    cleaned code up

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//console.log('Hello world!')
+// fuel
 const request = require("request-promise");
 const cheerio = require("cheerio");
 
-async function main() {
+async function getPriceData() {
     const result = await request.get(
         "https://www.roadrangerusa.com/fuel/check-fuel-prices"
     );
     const $ = cheerio.default.load(result);
     const scrapedRows = [];
     $("table tr").each((index, element) => {
+        if (index === 0) return true;
         const th = $(element).find("th");
         const location = th.text()
         const td = $(element).find("td").last();
@@ -19,4 +20,4 @@ async function main() {
     console.log(scrapedRows);
 
 }
-main();
+getPriceData();
