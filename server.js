d357603ee33ba27a8cf86586d3907babc6d676b0
alexible <aleksandrkr7k@gmail.com>
2021-11-18 20:31:12 -0800

    initial test setup

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ const express = require('express');
 const getPriceData = require('./get-prices-service')
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/api/prices', async (req, res) => {
     
     const data = await getPriceData();
     res.send(data)
 });
+
+app.get('/', async (req, res) => {
+    
+    const html = []
+    html.push("<h3>API Links</h3>");
+    html.push("<ul>");
+    html.push("<li>");
+    html.push("<a href='/api/prices'>");
+    html.push('get diesel prices');
+    html.push("</a>")
+    html.push("</li>");
+    html.push("</ul>");
+    res.send(html.join('\n'))
+});
+
+
 
 app.listen(3000);
 console.log("The server is listening on port 3000...")
